9784616084287d48d1010410d7fbee6816606e65
yjj <yjj@git.com>
2023-10-10 16:44:17 +0800

    second commit by idea

--- src/main/java/com/git/demo/GitTest.java
+++ src/main/java/com/git/demo/GitTest.java
@@ -3,5 +3,6 @@ package com.git.demo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git test");
+        System.out.println("hello git test2");
     }
 }
