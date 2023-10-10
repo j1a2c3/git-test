24bbbc38781946510dc2124315177c48a4c6bb66
yjj <yjj@git.com>
2023-10-10 19:52:46 +0800

    hot-fix first commit by idea

--- src/main/java/com/git/demo/GitTest.java
+++ src/main/java/com/git/demo/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello git test");
         System.out.println("hello git test2");
         System.out.println("hello git test3");
+        // idea-hot-fix分支提交
+        System.out.println("hello git test4");
     }
 }
